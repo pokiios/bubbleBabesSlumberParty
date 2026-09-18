dd8e54cf10399059d3ffab40ab362c93ff17cf8a
pokiios <2100571@uad.ac.uk>
2026-09-18 20:00:18 +0100

    Logic to change accessory type

--- Scripts/CharacterCustomiser.gd
+++ Scripts/CharacterCustomiser.gd
@@ -5,6 +5,7 @@ var accessoryColour : int
 var accessoryType : int
 var hairType : int
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	bodyColour = 0
@@ -20,21 +21,49 @@ func _process(delta: float) -> void:
 
 func _on_button_left_pressed() -> void:
 	if bodyColour <= 0:
-		bodyColour = 9
+		bodyColour = CharacterManager.colours.size()-1
 	else:
 		bodyColour -= 1
-	_set_materials()
+	_modify_character(false, false, false, true)
 
 
 func _on_button_right_pressed() -> void:
-	if bodyColour >= 9:
+	if bodyColour >= CharacterManager.colours.size()-1:
 		bodyColour = 0
 	else:
 		bodyColour += 1
-	_set_materials()
+	_modify_character(false, false, false, true)
 
-func _set_materials():
-	var charBodyColour = CharacterManager.colours.get(bodyColour)
-	$ChrBody.material.set("shader_parameter/my_colour", Vector4(charBodyColour.r, charBodyColour.g, charBodyColour.b, 1.0))
-	print(CharacterManager.colours[bodyColour])
+
+
+
+func _on_acessory_button_left_pressed() -> void:
+	if accessoryType >= CharacterManager.accessories.size()-1:
+		accessoryType = 0
+	else:
+		accessoryType += 1
+	_modify_character(false, true, false, false)
 	pass
+
+
+func _on_accessory_button_right_pressed() -> void:
+	if accessoryType <= 0:
+		accessoryType = CharacterManager.accessories.size()-1
+	else:
+		accessoryType -= 1
+	_modify_character(false, true, false, false)
+	pass # Replace with function body.
+
+func _modify_character(hair:bool, acc:bool, accColour: bool, bodColour: bool):
+	if (hair):
+		pass
+	elif (acc):
+		$ChrAccessory.texture = load(CharacterManager.accessories[accessoryType])
+		print($ChrAccessory.texture)
+	elif (accColour):
+		pass
+	elif (bodColour):
+		var charBodyColour = CharacterManager.colours[bodyColour]
+		$ChrBody.material.set("shader_parameter/my_colour", Vector4(charBodyColour.r, charBodyColour.g, charBodyColour.b, 1.0))
+		print(CharacterManager.colours[bodyColour])
+	
--- Scripts/CharacterManager.gd
+++ Scripts/CharacterManager.gd
@@ -1,3 +1,24 @@
 extends Node2D
 
-@onready var colours : Array[Color] = [Color("#EA6166"), Color("EA9961"), Color("#FFF38C"), Color("#A9FF8C"), Color("#8CFFF2"), Color("#8CB6FF"), Color("#BA8CFF"), Color("#FD8CFF"), Color("#EDF3F7"), Color("#453D51")]
+@onready var colours : Array[Color] = [
+Color("#EA6166"),
+Color("EA9961"),
+Color("#FFF38C"),
+Color("#A9FF8C"),
+Color("#8CFFF2"),
+Color("#8CB6FF"),
+Color("#BA8CFF"),
+Color("#FD8CFF"),
+Color("#EDF3F7"),
+Color("#453D51")
+]
+
+@onready var accessories : Array[String] = [
+"res://Assets/TEMP/Character/Accessories/acc_none.png", 
+"res://Assets/TEMP/Character/Accessories/acc_bobby_pins.png", 
+"res://Assets/TEMP/Character/Accessories/acc_bow.png",
+"res://Assets/TEMP/Character/Accessories/acc_cat_pin.png",
+"res://Assets/TEMP/Character/Accessories/acc_crossed_pins.png",
+"res://Assets/TEMP/Character/Accessories/acc_heart.png",
+"res://Assets/TEMP/Character/Accessories/acc_spiral.png"
+]
