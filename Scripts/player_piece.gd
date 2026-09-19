61bf984dfa3a7bf3a7a36e1d532c4333406e1467
pokemaz <98837743+MA-Briggs@users.noreply.github.com>
2026-09-19 19:02:30 +0100

    MB: sprite lerp works
    The lerping of sprite works with keyboard input

--- Scripts/player_piece.gd
+++ Scripts/player_piece.gd
@@ -2,10 +2,11 @@ class_name PlayerPiece extends Node3D
 
 var board_position : int = 0
 
-@onready var LerpTo : Marker3D = $LerpTo
+var lerpTo : Vector3
+var lerpFrom : Vector3
 @onready var sprite : Sprite3D = $Sprite3D
 var moveSprite : bool = false
-var dt = 0.0
+var t = 0.0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -13,16 +14,19 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	dt += delta * 0.4 
+	#print('lerpto: ' + str(lerpTo) +  ', lerpfrom: ' + str(lerpFrom) + ', sprite: ' + str(sprite.global_position) + ', t: ' + str(t))
 	if moveSprite:
-		sprite.position = sprite.position.lerp(LerpTo.position,dt)
-		if abs(sprite.position.x - LerpTo.position.x) <= 2 and abs(sprite.position.y - LerpTo.position.y)<= 2 and abs(sprite.position.z - LerpTo.position.z)<= 2:
+		t += delta * 0.8 
+		sprite.global_position = lerpFrom.lerp(lerpTo,t)
+		if t >= 1:
 			moveSprite = false
-			global_position = LerpTo.global_position
 			sprite.position = Vector3i(0,0,0)
-			LerpTo.global_position = global_position
+			t = 0
 	pass
 
 func move_on_board(boardPositionCoord):
-	LerpTo.global_position = boardPositionCoord
+	lerpTo = boardPositionCoord
+	lerpFrom = sprite.global_position
+	global_position = boardPositionCoord
+	sprite.global_position = lerpFrom
 	moveSprite = true
